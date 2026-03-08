01bd45b6ab69949df5361040e0b8b346660a3ece
Sadeepa Dinakara <sadeepa14dinakara@gmail.com>
2026-03-09 00:07:50 +0530

    first commit

---
+++ FOT_Event_Managment_System/src/main/java/com/example/FOT_Event_Managment_System/FotEventManagmentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.FOT_Event_Managment_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FotEventManagmentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FotEventManagmentSystemApplication.class, args);
+	}
+
+}
---
+++ FOT_Event_Managment_System/src/test/java/com/example/FOT_Event_Managment_System/FotEventManagmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FOT_Event_Managment_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FotEventManagmentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
